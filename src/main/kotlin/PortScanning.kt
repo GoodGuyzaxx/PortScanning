24ce71a303298c00591bd3385b5c00891f592db9
GoodGuyzaxx <drawww111@gmail.com>
2023-01-25 05:54:35 +0900

    Update: adding user input and remove error message when port is not open

--- src/main/kotlin/PortScanning.kt
+++ src/main/kotlin/PortScanning.kt
@@ -1,18 +1,19 @@
 import java.net.*
 
 fun main() {
+    print("Address: ")
+    val address = readln()
     //(21,80,443,53)
     val portList =  21..443
 
     for (port: Int in portList){
         try {
             val socket = Socket()
-            val netSocket = InetSocketAddress("192.168.1.1",port)
+            val netSocket = InetSocketAddress(address,port)
             socket.connect(netSocket,1000)
             socket.close()
             println("Port $port is open")
         }catch (error: Exception){
-            println("Port Error ${error.message}")
         }
     }
 }
